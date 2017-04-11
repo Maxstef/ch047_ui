0091ff2be884bb8c5d4c1fadfa10bf556621edd0
Maksym Stefanko <casstefaks@gmail.com>
2017-04-11 19:06:23 +0300

    work

--- client.js
+++ client.js
@@ -17,8 +17,6 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.static('*', {redirect: '/'});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
